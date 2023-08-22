286eeb09146a6af1a6fab9fb1de9910f27a64ba1
Mohammad Asif <asif.punchline@gmail.com>
2023-08-23 00:24:13 +0530

    ADDED: semi hosting feature is added

--- nazeer_stm32f0xx_drivers/Debug/Src/subdir.mk
+++ nazeer_stm32f0xx_drivers/Debug/Src/subdir.mk
@@ -6,17 +6,14 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../Src/main.c \
-../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
 ./Src/main.o \
-./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
 ./Src/main.d \
-./Src/syscalls.d \
 ./Src/sysmem.d 
 
 
@@ -27,7 +24,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
